4def3cb1a20326fafa4d9e611a0544c8a4da27ae
Roko K <70594433+RoccoCocco@users.noreply.github.com>
2024-03-15 23:39:34 +0100

    chore: remove files

--- src/utils/Refund.ts
+++
@@ -1,8 +0,0 @@
-import { Refund } from '../types/invoice';
-import { XmlRefund } from '../types/xmlInvoice';
-
-export const makeRefunds = (refound: Refund[]): XmlRefund[] =>
-  refound.map(({ Amount, Name }) => ({
-    IznosN: Amount.toFixed(2),
-    NazivN: Name,
-  }));
