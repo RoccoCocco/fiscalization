3dee6ba8bd905fde0f4dfb67981d19bcfcf62a20
Roko <roko.kokeza@gostudion.com>
2024-03-15 23:04:55 +0100

    chore: update before test

--- src/__tests__/Fiscalization.test.ts
+++ src/__tests__/Fiscalization.test.ts
@@ -14,15 +14,20 @@ const TEST_CERTIFICATE_PASS = 'apples';
 const TEST_CERTIFICATE_BUFFER = readFileSync(TEST_CERTIFICATE_PATH);
 
 describe('Fiscalization', () => {
+  let fiscalization: Fiscalization;
+
+  beforeEach(() => {
+    fiscalization = new Fiscalization(
+      TEST_CERTIFICATE_BUFFER,
+      TEST_CERTIFICATE_PASS
+    );
+  });
+
   it('XML should match with full data', () => {
     mockFormat
       .mockReturnValueOnce('01.01.2001T01:01:01')
       .mockReturnValueOnce('02.02.2002T02:02:02');
 
-    const fiscalization = new Fiscalization(
-      TEST_CERTIFICATE_BUFFER,
-      TEST_CERTIFICATE_PASS
-    );
     const xml = fiscalization.create(mockInvoice);
 
     expect(xml).toMatchSnapshot();
@@ -33,10 +38,6 @@ describe('Fiscalization', () => {
       .mockReturnValueOnce('01.01.2001T01:01:01')
       .mockReturnValueOnce('02.02.2002T02:02:02');
 
-    const fiscalization = new Fiscalization(
-      TEST_CERTIFICATE_BUFFER,
-      TEST_CERTIFICATE_PASS
-    );
     const xml = fiscalization.create(mockInvoiceAlternate);
 
     expect(xml).toMatchSnapshot();
