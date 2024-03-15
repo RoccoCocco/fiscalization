11cbfc9e7700eb0c10125220e1ad2b253e60e5fa
Roko K <70594433+RoccoCocco@users.noreply.github.com>
2024-03-15 23:32:36 +0100

    chore: removed files

--- src/enums/OrderOfNotes.ts
+++
@@ -1,4 +0,0 @@
-export enum OrderOfNotes {
-  BussinessUnit = 'P',
-  PaymentDevice = 'N',
-}
--- src/enums/PaymentTypes.ts
+++
@@ -1,7 +0,0 @@
-export enum PaymentTypes {
-  Card = 'K',
-  Cash = 'G',
-  Check = 'C',
-  Other = 'O',
-  Transfer = 'T',
-}
