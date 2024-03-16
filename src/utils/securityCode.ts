3e93fbe9ebcc50de0c4d11719e7af956998f02ed
Roko <roko.kokeza@gostudion.com>
2024-03-16 11:22:46 +0100

    feat: remove md5 package, use crypto

--- src/utils/securityCode.ts
+++ src/utils/securityCode.ts
@@ -1,4 +1,4 @@
-import md5 from 'md5';
+import { createHash } from 'crypto';
 
 import { Invoice } from '../types';
 import { toDateFormat } from './datetime';
@@ -7,15 +7,17 @@ export const createSecurityCode = (
   privateKey: string,
   data: Invoice
 ): string => {
-  const code = [
-    privateKey,
-    data.oib,
-    toDateFormat(data.dateTime),
-    data.billNumber.number,
-    data.billNumber.bussinessUnit,
-    data.billNumber.paymentDevice,
-    data.totalValue.toString(),
-  ];
-
-  return md5(code.join(''));
+  return createHash('md5')
+    .update(
+      [
+        privateKey,
+        data.oib,
+        toDateFormat(data.dateTime),
+        data.billNumber.number,
+        data.billNumber.bussinessUnit,
+        data.billNumber.paymentDevice,
+        data.totalValue.toString(),
+      ].join('')
+    )
+    .digest('hex');
 };
